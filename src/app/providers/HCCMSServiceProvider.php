f3dc7095025b156e87223f9750573e142af4f58a
Liutauras Kavaliauskas <liutauraskav@gmail.com>
2017-03-08 14:14:34 +0200

    Initial pacakge commit

---
+++ src/app/providers/HCCMSServiceProvider.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace interactivesolutions\honeycombcms\app\providers;
+
+use Illuminate\Support\ServiceProvider;
+use interactivesolutions\honeycombacl\app\providers\HCACLServiceProvider;
+use interactivesolutions\honeycombcore\providers\HCCoreServiceProvider;
+use interactivesolutions\honeycombcoreui\providers\HCCoreUiServiceProvider;
+use interactivesolutions\honeycomblanguages\app\providers\HCLanguagesServiceProvider;
+use interactivesolutions\honeycombregions\app\providers\HCRegionsServiceProvider;
+use interactivesolutions\honeycombresources\app\providers\HCResourcesServiceProvider;
+use interactivesolutions\honeycombscripts\app\providers\HCScriptsServiceProvider;
+use interactivesolutions\honeycombslugs\app\providers\HCSlugsServiceProvider;
+use interactivesolutions\honeycomburlshortener\app\providers\HCURLShortenerServiceProvider;
+
+class HCCMSServiceProvider extends ServiceProvider
+{
+    /**
+     * Register commands
+     *
+     * @var array
+     */
+    protected $commands = [];
+
+    protected $namespace = 'interactivesolutions\honeycombcms\app\http\controllers';
+
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        // register artisan commands
+        $this->commands($this->commands);
+
+        // loading views
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'HCCMS');
+
+        // loading translations
+        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'HCCMS');
+
+        // registering elements to publish
+        $this->registerPublishElements();
+
+        // registering helpers
+        $this->registerHelpers();
+
+        // registering routes
+        $this->registerRoutes();
+
+        //register providers
+        $this->registerProviders();
+    }
+
+    /**
+     * Register helper function
+     */
+    private function registerHelpers()
+    {
+        $filePath = __DIR__ . '/../http/helpers.php';
+
+        if (\File::isFile($filePath))
+            require_once $filePath;
+    }
+
+    /**
+     *  Registering all vendor items which needs to be published
+     */
+    private function registerPublishElements ()
+    {
+        $directory = __DIR__ . '/../../database/migrations/';
+
+        // Publish your migrations
+        if (file_exists ($directory))
+            $this->publishes ([
+                __DIR__ . '/../../database/migrations/' => database_path ('/migrations'),
+            ], 'migrations');
+
+        $directory = __DIR__ . '/../public';
+
+        // Publishing assets
+        if (file_exists ($directory))
+            $this->publishes ([
+                __DIR__ . '/../public' => public_path ('honeycomb'),
+            ], 'public');
+    }
+
+    /**
+     * Registering routes
+     */
+    private function registerRoutes()
+    {
+        $filePath = __DIR__ . '/../../app/honeycomb/routes.php';
+
+        if ($filePath)
+            \Route::group (['namespace' => $this->namespace], function ($router) use ($filePath) {
+                require $filePath;
+            });
+    }
+
+    /**
+     * Registering 3rd party providers which are required for this package to run
+     */
+    private function registerProviders()
+    {
+        
+    }
+}
+
+
---
+++ src/database/seeds/HoneyCombDatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+namespace interactivesolutions\honeycombcms\database\seeds;
+
+use Illuminate\Database\Seeder;
+
+class HoneyCombDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
